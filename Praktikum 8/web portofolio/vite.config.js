3e95fbcfa45d2ec4edc29ce0e230599e984234e3
Ahmad Raka Putra Pratamaa <2400016089@webmail.uad.ac.id>
2025-12-18 15:37:48 +0700

    Update praktikum 8 (webportofolio)

--- Praktikum 8/web portofolio/vite.config.js
+++ Praktikum 8/web portofolio/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Tambahkan baris base di bawah ini
-  // Sesuaikan dengan nama repository GitHub Anda persis
-  base: '/24000016089_Ahmad-Raka-Putra-Pratama_A/',
+  // Pastikan base sesuai dengan nama repository baru Anda
+  base: '/Praktikum-Tekweb/',
 })
